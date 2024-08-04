6ea6a678b46d5315131481004aebf099a8e96d4d
lutencjusz <lutencjusz@gmail.com>
2024-08-04 09:18:04 +0200

    Init

--- src/main/java/org/acme/Account.java
+++ src/main/java/org/acme/Account.java
@@ -3,5 +3,9 @@ package org.acme;
 import java.util.UUID;
 
 public record Account(UUID id, String name, String accountNumber) {
-    public Account {}
+    /**
+     * Konstruktor bezparametrowy klasy Account - żeby działało serializowanie do JSON.
+     */
+    public Account {
+    }
 }
