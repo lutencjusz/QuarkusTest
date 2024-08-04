50e875b0d9e2362970a443aada864d6cc904aa92
lutencjusz <lutencjusz@gmail.com>
2024-08-04 21:53:33 +0200

    Dodanie Swagger UI

--- src/main/java/org/acme/AccountController.java
+++ src/main/java/org/acme/AccountController.java
@@ -4,6 +4,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 /**
  * AccountController jest klasą, która obsługuje żądania HTTP związane z kontami.
@@ -20,18 +24,29 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Tag(name = "get", description = "Pobierz dane o koncie/kontach")
     @GET
     public Response getAllAccounts() {
         return Response.ok(accountService.getAllAccounts()).build();
     }
 
+    @Tag(name = "get", description = "Pobierz dane o koncie/kontach")
     @GET
     @Path("/{name}")
-    public Response getAccount(@PathParam("name") String name) {
+    public Response getAccount(
+            @Parameter(description = "Nazwa konta, którą chcesz pobrać.", required = true, example = "Bob")
+            @PathParam("name") String name) {
         return Response.ok(accountService.getAccount(name)).build();
     }
-
+    @Tag(name = "post", description = "Dodaj nowe konto")
     @POST
+    @Operation(
+            summary = "Dodaj nowe konto",
+            description = "Dodaj nowe konto do listy kont.",
+            operationId = "addAccount"
+    )
+    @APIResponse(responseCode = "200", description = "Konto zostało dodane.")
+    @APIResponse(responseCode = "400", description = "Niepoprawne dane konta.")
     public Response addAccount(Account account) {
         return Response.ok(accountService.addAccount(account)).build();
     }
