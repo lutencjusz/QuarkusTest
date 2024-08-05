7875aa130c7af6526a5c35d45c00b0f545f41c4f
lutencjusz <lutencjusz@gmail.com>
2024-08-05 22:16:00 +0200

    Dodanie assercji miękkich i twardych

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -1,22 +1,47 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.assertj.core.api.SoftAssertions;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 @QuarkusTest
 class GreetingResourceTest {
 
     @Test
     void testAccountGetAllAccounts() {
-        given()
+        JsonPath response = RestAssured.given()
                 .when()
                 .get("/accounts")
                 .then()
                 .statusCode(200)
-                .body(containsString("Alice"));
+                .body(notNullValue(),
+                        containsString("Alice"),
+                        containsString("Bob"))
+                .body("size()", greaterThanOrEqualTo(2))
+                .extract().body().jsonPath();
+        List<Account> accounts = response.getList("", Account.class);
+        Account account = accounts.stream()
+                .filter(i -> i.name().equals("Alice"))
+                .findFirst()
+                .orElse(null);
+        System.out.println(account);
+        MatcherAssert.assertThat("Sprawdzenie nazwy konta", response.getString("name"), containsString("Alice"));
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(response.getString("name")).as("Sprawdzenie nazwy projektu").containsOnlyOnce("Alice");
+        softAssertions.assertThat(response.getString("name")).as("Sprawdzenie nazwy projektu").containsOnlyOnce("Bob");
+        softAssertions.assertThat(response.getString("name")).as("Sprawdzenie, czy nazwa jest pusta").isNotBlank();
+        softAssertions.assertThat(response.getString("name")).as("Sprawdzenie, czy nazwa jest pusta").isNotEmpty();
+        softAssertions.assertAll();
     }
 
     @Test
